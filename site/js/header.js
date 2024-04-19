494b492b659c0b405f0cfb43fe3e8f5a1ed1f017
jhpriestley16 <jaydenUX@outlook.com>
2024-04-19 14:20:26 +0100

    local commit
    trying to fix the header gone wrong

--- site/js/header.js
+++ site/js/header.js
@@ -1,17 +1,20 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 75) {
-    document.querySelector("header").style.height = "75px"
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 50) {
+    document.querySelector("header").style.height = "50px"
     document.querySelector(".header-content").style.flexDirection = "row";
     document.querySelector(".header-content").style.justifyContent = "space-between";
     document.querySelector("h1").style.fontSize = "1.5em";
     document.querySelector(".title-description-wrapper").style.display = "none";
+    document.getElementsById("page-links").style.background = "transparent";
 
   } else {
     document.querySelector("header").style.height = "300px";
     document.querySelector(".header-content").style.flexDirection = "column";
+    document.querySelector(".header-content").style.justifyContent = "center";
     document.querySelector("h1").style.fontSize = "3.5em";
     document.querySelector(".title-description-wrapper").style.display = "block";
+    document.getElementById("page-links").style.background = "#1a1a1a";
   }
 }
