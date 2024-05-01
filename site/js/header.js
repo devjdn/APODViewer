adef2da407c8f35d14ee84a98ad733b4981da037
jhpriestley16 <153106449+jhpriestley16@users.noreply.github.com>
2024-05-01 23:59:24 +0000

    home commit, functional dropdown implemented

--- site/js/header.js
+++ site/js/header.js
@@ -1,20 +1,11 @@
-window.onscroll = function() {scrollFunction()};
+document.addEventListener('DOMContentLoaded', function() {
+  const dropdownButton = document.getElementById('dropdownButton');
+  const dropdownMenu = document.getElementById('dropdownMenu');
 
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 50) {
-    document.querySelector("header").style.height = "50px"
-    document.querySelector(".header-content").style.flexDirection = "row";
-    document.querySelector(".header-content").style.justifyContent = "space-between";
-    document.querySelector("h1").style.fontSize = "1.5em";
-    document.querySelector(".title-description-wrapper").style.display = "none";
-    document.getElementsById("page-links").style.background = "transparent";
-
-  } else {
-    document.querySelector("header").style.height = "300px";
-    document.querySelector(".header-content").style.flexDirection = "column";
-    document.querySelector(".header-content").style.justifyContent = "center";
-    document.querySelector("h1").style.fontSize = "3.5em";
-    document.querySelector(".title-description-wrapper").style.display = "block";
-    document.getElementById("page-links").style.background = "#1a1a1a";
-  }
-}
+  // Toggle dropdown menu
+  dropdownButton.addEventListener('click', function() {
+      const expanded = dropdownButton.getAttribute('aria-expanded') === 'true';
+      dropdownButton.setAttribute('aria-expanded', String(!expanded));
+      dropdownMenu.setAttribute('aria-hidden', String(expanded));
+  });
+});
