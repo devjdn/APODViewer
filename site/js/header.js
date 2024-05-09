e00dacd10f7e58cd09af45a81ae114e75fd12385
jhpriestley16 <jaydenUX@outlook.com>
2024-05-09 16:30:51 +0100

    local commit
    added a button that takes users back to the top of the page when they scroll past 20px

--- site/js/header.js
+++ site/js/header.js
@@ -9,3 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
       dropdownMenu.setAttribute('aria-hidden', String(expanded));
   });
 });
+
+  // Get the button:
+let toTop = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
