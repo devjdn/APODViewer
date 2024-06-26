12b195c10ed7d0dc9b9142e1182d679b773a6cc6
arraydevbackup <154961558+arraydevbackup@users.noreply.github.com>
2024-06-26 19:11:21 +0000

    adding some more feature descriptions

--- space/app/page.tsx
+++ space/app/page.tsx
@@ -2,10 +2,37 @@ import Apod from "./api/daily-api";
 
 export default function Home() {
   return (
-    <div className="home">
+    <div className="content">
       <div className="site-introduction-text">
-          <h1>The cosmos at the click of a finger.</h1>
+          <h1>Look into the past.</h1>
+          <h3>View NASA's library of history with SpaceScope</h3>
       </div>
+      <section className="landing-nav">
+        <article>
+          <h3>Daily APOD</h3>
+          <p>View NASA's picture of the day from today.</p>
+        </article>
+        <article>
+          <h3>APOD Time Machine</h3>
+          <p>Select a date and look back at the APOD from that date.</p>
+        </article>
+        <article>
+          <h3>APOD Library</h3>
+          <p>View an array of randomly fetched APODs from NASA's database.</p>
+        </article>
+        <article>
+          <h3>Mars Rovers</h3>
+          <p>Take a look at the different rovers that have been trailblazing on Mars.</p>
+        </article>
+        <article>
+          <h3>Daily Mars Photo</h3>
+          <p>View an array of randomly fetched APODs from NASA's database.</p>
+        </article>
+        <article>
+          <h3>Rover Photo Search</h3>
+          <p>View an array of randomly fetched APODs from NASA's database.</p>
+        </article>
+      </section>
       <Apod />
     </div>
   );
