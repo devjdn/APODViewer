4b18f65a98bc2097a1a4eccb408cfcecc5e31672
jhpriestley16 <153106449+jhpriestley16@users.noreply.github.com>
2024-07-23 00:53:35 +0000

    removing any trace of next js LMAO

--- space/app/ui/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return(
-        <footer>
-            <div className="logo">
-                <a href="/"><p>SpaceScope</p></a>
-            </div>
-        </footer>
-    );
-}
